f5236cce29c116b7f86cce1f83e4b6fb42be251c
Chrisglz14 <Christiang14@hotmail.com.ar>
2025-02-07 06:29:15 -0300

    mejoras

--- src/layouts/BannerImg.tsx
+++ src/layouts/BannerImg.tsx
@@ -3,9 +3,10 @@ export const BannerImg = () => {
   return (
     <div className={styles.hero_banner}>
         <div className={styles.overlay}>
-            <h1 className="title">Bienvenidos a la Tienda!</h1>
-            <p className="pagraph">La mejor ropa de todo Santa Fe</p>
-            <img src="src\assets\bg-img\backimg.jpg" alt="imagen1"/>
+              <div className={styles.textBox}>
+                <h1 className="title">Bienvenidos a la Tienda!</h1>
+                <p className="pagraph">La mejor ropa de todo Santa Fe</p>
+              </div>
         </div>
     </div>
   )
